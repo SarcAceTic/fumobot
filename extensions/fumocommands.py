a7639878e83d226263b1d19de3f29875b6fe2e43
SarcAceTic <sarcacetic@gmail.com>
2025-01-26 10:32:03 +0700

    Added BJ view, commands, and emojis

--- extensions/fumocommands.py
+++ extensions/fumocommands.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ui import View, Button
 from io import BytesIO
 from discord.ext import commands
 import aiohttp
@@ -8,7 +9,7 @@ import asyncio
 import asqlite
 from datetime import timedelta, datetime
 import random
-from PIL import Image
+import pydealer as pd
 
 async def fumoDbCheck():
   async with asqlite.connect("fumo.db") as db:
@@ -21,6 +22,94 @@ async def fumoDbCheck():
       )
       """)
       await db.commit()
+
+class BlackjackView(View):
+  def __init__(self, ctx, hand, dealerHand, embed, deck, bet, pscore, timeout = 20):
+    super().__init__(timeout=timeout)
+    self.ctx = ctx
+    self.hand = hand
+    self.dealerHand = dealerHand
+    self.embed = embed
+    self.deck = deck
+    self.bet = bet
+    self.pscore = pscore
+
+    self.winnings = bet
+    self.gameRover = False
+
+  async def interaction_check(self, interaction):
+    if interaction.user.id != self.ctx.user.id:
+      await interaction.response.send_message("This button is not for you!!", ephemeral=True)
+      return False
+    else:
+      return True
+
+  async def on_timeout(self):
+    if not self.gameRover:
+      for item in self.children:
+        item.disabled= True
+
+      await self.message.edit(view=self)
+      await self.ctx.reply("You didn't respond in time! The whole game is cancelled, and fortunately, the dealer was kind enough to give you your money back.")
+  
+  @discord.ui.button(label="Hit!", style=discord.ButtonStyle.green)
+  async def hitButton(self, interaction: discord.Interaction, button: Button):
+    self.hand.add(self.deck.deal(1))
+    self.pscore = customutilities.handValue(self.hand)
+
+    self.embed = discord.Embed(
+      description=f"{f"Oh no! You busted! You lost {self.bet}...\n" if self.pscore > 21 else ""}**Your Hand**: {" ".join(f"| {card.value} of {card.suit} " for card in self.hand)}\nScore: {self.pscore}\n\n**Dealer's Hand**: Hidden | {" ".join(f"{card.value} of {card.suit} | " for card in self.dealerHand[1:])}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    self.embed.set_footer(
+      text=self.ctx.author.name,
+      icon_url=self.ctx.author.avatar.url
+    )
+
+    if self.pscore > 21:
+      await customutilities.updateBalance(self.ctx.author.id, self.bet * -1)
+      self.gameOver()
+      self.gameRover = True
+
+    await interaction.response.edit_message(embed=self.embed, view=self)
+  
+  @discord.ui.button(label="Stand!", style=discord.ButtonStyle.red)
+  async def standButton(self, interaction: discord.Interaction, button: Button):
+    dealerScore = customutilities.handValue(self.dealerHand)
+
+    while customutilities.handValue(self.dealerHand) < 17:
+      self.dealerHand.add(self.deck.deal(1))
+      dealerScore = customutilities.handValue(self.dealerHand)
+    
+    print(self.pscore)
+    if dealerScore > self.pscore and (dealerScore < 21):
+      await customutilities.updateBalance(self.ctx.author.id, self.bet * -1)
+      self.gameOver()
+      self.gameRover = True
+    elif dealerScore == self.pscore:
+      self.gameOver()
+      self.gameRover = True
+    else:
+      await customutilities.updateBalance(self.ctx.author.id, self.winnings)
+      self.gameOver()
+      self.gameRover = True
+    
+    self.embed = discord.Embed(
+      description=f"{f"Ahh! You lost! You gave away {self.bet} to the dealer!" if dealerScore > self.pscore and (dealerScore < 21) else f"You drew with the dealer! You didn't win anything..." if dealerScore == self.pscore else f"Yay! The dealer {"busted" if dealerScore > 21 else "lost"}! You won {self.winnings}"}\n\n**Your Hand**: {" ".join(f"| {card.value} of {card.suit} " for card in self.hand)}\nScore: {self.pscore}\n\n**Dealer's Hand**: {" ".join(f"| {card.value} of {card.suit}" for card in self.dealerHand)}\nScore:{customutilities.handValue(self.dealerHand)}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    self.embed.set_footer(
+      text=self.ctx.author.name,
+      icon_url=self.ctx.author.avatar.url
+    )
+
+    await interaction.response.edit_message(embed=self.embed, view=self)
+  
+  def gameOver(self):
+    self.standButton.disabled = True
+    self.hitButton.disabled = True
 
 class FumoCommands(commands.Cog, name="Fumo"):
   """Take care of your Fumos here in this simple pet economy game!"""
@@ -118,6 +207,49 @@ class FumoCommands(commands.Cog, name="Fumo"):
 
     await customutilities.updateBalance(ctx.author.id, winnings)
     await rolled.edit(embed=rolledEmbed)
+  
+  @commands.command(name="blackjack", aliases=["bj"])
+  async def blackjack(self, ctx, power: int = commands.parameter(description="The amount of power to be put on bet.")):
+    """Gambles your power away in a game of Blackjack. Pays out 2x, and Blackjacks pays out 2.5x"""
+    await customutilities.lowBalance(ctx, power=power)
+    bjEmbed = discord.Embed(
+      description=f"Dealing cards...",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    bjEmbed.set_footer(
+      text=ctx.author.name,
+      icon_url=ctx.author.avatar.url
+    )
+
+    bjMessage = await ctx.reply(embed=bjEmbed)
+    await asyncio.sleep(2)
+
+    bjDeck = pd.Deck()
+    bjDeck.shuffle()
+    
+    playerHand = bjDeck.deal(2)
+    dealerHand = bjDeck.deal(2)
+    
+    initialScore = customutilities.handValue(playerHand)
+    
+    bjEmbed = discord.Embed(
+      description=f"{"**BLACKJACK!**\n" if initialScore == 21 else ""}**Your Hand**: {" ".join(f"{card.value} of {card.suit} | " for card in playerHand)}\nScore: {initialScore}\n\n**Dealer's Hand**: Hidden | {" ".join(f"{card.value} of {card.suit} | " for card in dealerHand[1:])}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    bjEmbed.set_footer(
+      text=ctx.author.name,
+      icon_url=ctx.author.avatar.url
+    )
+
+    view = BlackjackView(ctx, playerHand, dealerHand, bjEmbed, bjDeck, power, initialScore)
+
+    if initialScore == 21:
+      view.winnings = power + int((power * 0.5))
+
+    view.message = await bjMessage.edit(embed=bjEmbed, view=view)
+
 
 async def setup(bot):
   await bot.add_cog(FumoCommands(bot))
